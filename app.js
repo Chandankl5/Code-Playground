8c78bee05f65a3cd20b75206fb4a7c407a2500d4
chandan <chandankl0310@gmail.com>
2023-07-21 21:09:03 +0530

    Update route handling or assets

--- app.js
+++ app.js
@@ -3,7 +3,15 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(path.resolve()))
+app.get('/index.js', (req, res)  => {
+  res.sendFile(path.resolve('js') + '/index.js')
+})
+
+app.get('/index.css', (req, res)  => {
+  res.sendFile(path.resolve('css') + '/index.css')
+})
+
+app.use('/index.css', express.static(path.resolve('css')))
 
 app.get('/', (req, res) => {
   res.sendFile(path.resolve() + '/index.html');
